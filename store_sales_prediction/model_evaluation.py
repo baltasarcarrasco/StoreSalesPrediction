8b70a20c724341771822da61a9f623c8bcd2c585
dleonrdz <diego.leon07@outlook.com>
2024-03-18 17:45:33 +0100

    model_evaluation inclusion of metrics summary

--- store_sales_prediction/model_evaluation.py
+++ store_sales_prediction/model_evaluation.py
@@ -1,24 +1,52 @@
 import pandas as pd
-from sklearn.metrics import mean_squared_error
 from db_utilities import write_table, read_table
+from sklearn.metrics import mean_squared_error, root_mean_squared_error, mean_absolute_error, mean_absolute_percentage_error
 import joblib
 
+eval_metrics = {'mse':mean_squared_error,
+                'rmse':root_mean_squared_error,
+                'mae':mean_absolute_error,
+                'mape':mean_absolute_percentage_error}
+
 # Load the saved model
-model = joblib.load("../models/store_sales_model.pkl")
+model = joblib.load("./models/store_sales_model.pkl")
 
 # Load the test data
 test = read_table("sales_test")
+train = read_table("sales_train")
 
 # Define the features and target variable
 X_test = test.drop(["id", "date", "sales"], axis=1)
 y_test = test["sales"]
+X_train = train.drop(["id", "date", "sales"], axis=1)
+y_train = train["sales"]
 
 # Make predictions
 y_pred = model.predict(X_test)
+y_pred_train = model.predict(X_train)
 
 # Evaluate the model
-rmse = mean_squared_error(y_test, y_pred, squared=False)
 
-# Save predictions and actual values to the database
+metrics_list = []
+train_scores = []
+test_scores = []
+for metric, function in eval_metrics.items():
+    train_metric = function(y_train, y_pred_train)
+    test_metric = function(y_test, y_pred)
+    metrics_list.append(metric)
+    train_scores.append(train_metric)
+    test_scores.append(test_metric)
+
+metrics_df = pd.DataFrame({'metric':metrics_list,
+                           'train':train_scores,
+                           'test':test_scores})
+
+#rmse = mean_squared_error(y_test, y_pred, squared=False)
+
+# Save predictions, actual values and metrics to the database
 results = pd.DataFrame({"id": test["id"], "sales_pred": y_pred, "sales_actual": y_test})
+results_train = pd.DataFrame({"id": train["id"], "sales_pred": y_pred_train, "sales_actual": y_train})
 write_table(results, "sales_predictions")
+write_table(results_train, "sales_predictions_train")
+write_table(metrics_df, "metrics_summary")
+
