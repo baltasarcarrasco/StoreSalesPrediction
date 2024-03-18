f03cf209cc4a67dc29d24671580de3f37f579b75
baltasarcarrasco <88855315+baltasarcarrasco@users.noreply.github.com>
2024-03-18 20:17:44 +0100

    Changes in Typer App to get commands to work

--- cli/train.py
+++ cli/train.py
@@ -5,10 +5,17 @@ app = typer.Typer()
 
 
 @app.command(help="Trains the sales prediction model.")
-def train():
+def train(
+    use_xgbooster: bool = typer.Option(
+        ...,
+        "--xgboost",
+        "-x",
+        help="Choose XGBoost as the model. Default is Random Forest.",
+    )
+):
     """
     Trains the sales prediction model.
     """
     typer.echo("Training the model...")
-    train_model()
+    train_model(user_requested_model=True, use_xgbooster=use_xgbooster)
     typer.echo("Model trained successfully!")
