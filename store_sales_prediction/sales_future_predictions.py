b6b8a402c78b560f88435e2b257ad6ae072eced2
Diego León <diego.leon07@outlook.com>
2024-03-20 07:42:41 +0100

    predictions utilities

--- store_sales_prediction/sales_future_predictions.py
+++ store_sales_prediction/sales_future_predictions.py
@@ -1,8 +1,8 @@
 from store_sales_prediction.db_utilities import read_table, write_table
-from store_sales_prediction.store_model_future_predictions_utilities import iterative_predictions_optimized
+from store_sales_prediction.store_model_future_predictions_utilities import iterative_predictions
 import joblib
 
 df_sales = read_table('sales')
 trained_model = joblib.load("./models/store_sales_model.pkl")
-predictions = iterative_predictions_optimized(trained_model, sales_df=df_sales, input_date='2017-11-30')
+predictions = iterative_predictions(trained_model, sales_df=df_sales, input_date='2017-11-30')
 write_table(predictions, 'predicted_sales')
